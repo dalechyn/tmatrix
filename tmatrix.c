6d6ccc88aca6244bfab68dbf25d65d724cb77cce
hotwater <hotwater438@tutanota.com>
2019-08-16 02:09:42 +0300

    Rainbow matrix added

--- tmatrix.c
+++ tmatrix.c
@@ -12,9 +12,8 @@
 #include <stdbool.h>
 
 #define symb_count 70
-#define colors_count 3
 
-#define VERSION "v0.1"
+#define VERSION "v0.2"
 
 #define COLOR_GREEN "\033[00;38;05;46m"
 #define COLOR_BOLD_WHITE "\033[01;38;05;15m"
@@ -26,28 +25,30 @@
 #define COLOR_BLUE "\033[38;05;68m"
 #define COLOR_AQUA "\033[38;05;51m"
 
+static const char* COLORS_RAINBOW[] =  { "\033[38;05;196m", "\033[38;05;226m", "\033[00;38;05;46m", "\033[38;05;68m", "\033[38;05;129m" };
+
 static volatile int running = 1;
 
-void getTermSize(int *x, int *y) {
+void get_term_size(int*x, int*y) {
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    *x = w.ws_col;
-    *y = w.ws_row;
+   *x = w.ws_col;
+   *y = w.ws_row;
 }
 
-void intHandler(int dummy) {
+void int_handler(int dummy) {
 	int x, y;
 	system("tput reset");
     running = 0;   
 }
 
-struct Digital {
+struct Line {
     int x;
     int head;
     int length;
     int tick;
     int speed;
-    wchar_t * data;
+    wchar_t* data;
 };
 
 void printHelp() {
@@ -60,6 +61,7 @@ void printHelp() {
     printf("\t\t-w: WHITE\n");
     printf("\t\t-r: RED\n");
     printf("\t\t-y: YELLOW\n");
+    printf("\t\t--rainbow: rainbow\n");
     printf("\tProgram options:\n");
     printf("\t\t--chance [number]: Set random symbol chance spawn (1/chance)\n");
     printf("\t\t--speed [number]: Set speed of tmatrix (ms)\n");
@@ -69,142 +71,142 @@ void printHelp() {
     printf("\t\t-v: Version\n");
 }
 
-int areEqual(char * s1, char * s2) {
+int are_equal(char* s1, char* s2) {
     return strcmp(s1, s2) == 0;
-}
-
-void colorizeSymbol(wchar_t * destination, wchar_t c, wchar_t * prefix) {
-    const int sizePrefix = wcslen(prefix) * sizeof(wchar_t);
-    memcpy(destination, prefix, sizePrefix);
-    destination[sizePrefix] = c;
-    //destination[sizePrefix + 1] = L'\n';
 }
 
 int get_rand_in_range(int min, int max) {
     return (random() % ((max + 1) - min) + min);
 }
 
-wchar_t randomChar(wchar_t chars[symb_count]) {
+wchar_t random_char(wchar_t chars[symb_count]) {
     return chars[get_rand_in_range(0, symb_count - 1)];
 }
 
-wchar_t * generateRain(wchar_t chars[symb_count], int length) {
-    wchar_t * arr = malloc(length * sizeof(wchar_t) + 1);
-    for(int i = 0; i < length; i++)
-        arr[i] = randomChar(chars);
+wchar_t* generate_rain(wchar_t chars[symb_count], int length) {
+    wchar_t* arr = malloc(length* sizeof(wchar_t) + 1);
+    for (int i = 0; i < length; i++)
+        arr[i] = random_char(chars);
         arr[length] = L'\0';
     return arr;
 }
 
-void outSymb(int x, int y, char * prefix, wchar_t symb) {
+void out_symb(int x, int y, const char* prefix, wchar_t symb) {
     wprintf(L"\n\033[%d;%dH%s%lc\n", y, x, prefix, symb);
 }
 
-void randomizeDL(struct Digital * line, wchar_t chars[symb_count], int y_size, char * accent, int symbolChangeChance) {
-    for(int i = 0; i < line->length; i++)
-        //mixing chars with 5% chance, also check if it's visible
-        if(get_rand_in_range(0, symbolChangeChance - 1) == 0 && line->head - i > 0 && line->head - i < y_size - 1) {
-            line->data[i] = randomChar(chars);
-            //redraw it 
-            outSymb(line->x, line->head - i, (i > 2 ? accent: (i > 0 ? COLOR_WHITE  : COLOR_BOLD_WHITE)), line->data[i]);
+void line_randomize(struct Line* line, wchar_t chars[symb_count], int y_size, char* accent, int symb_change_change, int is_rainbow) {
+    for (int i = 0; i < line->length; i++)
+        // mixing chars with 5% chance, also check if it's visible
+        if (get_rand_in_range(0, symb_change_change - 1) == 0 && line->head - i > 0 && line->head - i < y_size - 1) {
+            line->data[i] = random_char(chars);
+            // redraw it 
+			if (is_rainbow)
+				out_symb(line->x, line->head - i, (i > 2 ? COLORS_RAINBOW[get_rand_in_range(0, 4)] : (i > 0 ? COLOR_WHITE  : COLOR_BOLD_WHITE)), line->data[i]);
+            else 
+				out_symb(line->x, line->head - i, (i > 2 ? accent: (i > 0 ? COLOR_WHITE : COLOR_BOLD_WHITE)), line->data[i]);
         }
 }
 
-void moveAndDrawDL(struct Digital * line, int y_size, wchar_t chars[symb_count], char * accent, int symbolChangeChance) {
+void line_tick(struct Line* line, int y_size, wchar_t chars[symb_count], char* accent, int symb_change_change, int is_rainbow) {
     line->tick++;
     int willMove = 0;
-    //draw operations are made on 0-4st elements in line
-    if(line->tick == line->speed) {
+    if (line->tick == line->speed) {
         willMove = 1;
         line->head++;
         line->tick = 0;
     }
 
-  //check if line hasn't gone past bottom of the terminal
-    if(line->head - line->length >= y_size - 1) {
-        //if so respawn
-        //free its data
+    if (line->head - line->length >= y_size - 1) {
+        // if so respawn
+        // free its data
         int l = get_rand_in_range(5, y_size / 2);
         free(line->data);
         line->length = l;
         line->head = get_rand_in_range(-30, -1);
         line->speed = get_rand_in_range(1, 6);
         line->tick = 0;
-        line->data = generateRain(chars, l);
+        line->data = generate_rain(chars, l);
     }
 
-    //mixing DL
-    randomizeDL(line, chars, y_size, accent, symbolChangeChance);
-    if(line->head > 0 && willMove) {
-        //move all symbols by 1 to create empty space at the 0
-        for(int i = line->length - 1; i >= 1; i--) line->data[i] = line->data[i - 1];
-        line->data[0] = randomChar(chars);
-        //printing first white bold symbol
-        if(line->head > 0 && line->head < y_size - 1)
-            outSymb(line->x, line->head, COLOR_BOLD_WHITE, line->data[0]);
-        //next two symbols are also white but not bold
-        if(line->head - 1 > 0 && line->head - 1 < y_size - 1)
-            outSymb(line->x,line->head - 1, COLOR_WHITE, line->data[1]);
+    line_randomize(line, chars, y_size, accent, symb_change_change, is_rainbow);
 
-        if(line->head - 2 > 0 && line->head - 2 < y_size - 1)
-            outSymb(line->x, line->head - 2, COLOR_WHITE, line->data[2]);
+    if (line->head > 0 && willMove) {
+        // move all symbols by 1 to create empty space at the 0
+        for (int i = line->length - 1; i >= 1; i--) line->data[i] = line->data[i - 1];
+        line->data[0] = random_char(chars);
+        // printing first white bold symbol
+        if (line->head > 0 && line->head < y_size - 1)
+            out_symb(line->x, line->head, COLOR_BOLD_WHITE, line->data[0]);
+        // next two symbols are also white but not bold
+        if (line->head - 1 > 0 && line->head - 1 < y_size - 1)
+            out_symb(line->x,line->head - 1, COLOR_WHITE, line->data[1]);
 
-        //printing green symb
-        if(line->head - 3 > 0 && line->head - 3 < y_size - 1)
-            outSymb(line->x, line->head - 3, accent, line->data[3]);
+        if (line->head - 2 > 0 && line->head - 2 < y_size - 1)
+            out_symb(line->x, line->head - 2, COLOR_WHITE, line->data[2]);
 
-        //erasing last symb and checking if last symbol not visible - redefine line
-        if(line->head - line->length > 0) {
-            outSymb(line->x, line->head - line->length, "", L' ');
+        // printing green symb
+        if (line->head - 3 > 0 && line->head - 3 < y_size - 1)
+			if (is_rainbow)
+            	out_symb(line->x, line->head - 3, COLORS_RAINBOW[get_rand_in_range(0, 4)], line->data[3]);
+			else
+            	out_symb(line->x, line->head - 3, accent, line->data[3]);
+
+        // erasing last symb and checking if last symbol not visible - redefine line
+        if (line->head - line->length > 0) {
+            out_symb(line->x, line->head - line->length, "", L' ');
         }    
     }
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char** argv) {
     int argi = 1;
-    char * accent = COLOR_GREEN;
-    int symbolChangeChance = 20;
+    char* accent = COLOR_GREEN;
+    int symb_change_change = 20;
     int dlLength = -1;
     int dlLengthSet = 0;
     int refreshDelay = 50000;
+	int is_rainbow = 0;
     while(argi < argc) {
-        if(areEqual(argv[argi], "--help")) {
+        if (are_equal(argv[argi], "--help")) {
             printHelp();
             return 0;
-        } else if(areEqual(argv[argi], "-v")) {
-            printf("TMatrix, version %s, created by Vladislav Dalechyn\n", VERSION);
+        } else if (are_equal(argv[argi], "-v")) {
+            printf("TMatrix, version %s, created by Vladyslav Dalechyn\n", VERSION);
             return 0;
-        } else if(areEqual(argv[argi], "-r")) {
+        } else if (are_equal(argv[argi], "-r")) {
             accent = COLOR_RED;
-        } else if(areEqual(argv[argi], "-b")) {
+        } else if (are_equal(argv[argi], "-b")) {
             accent = COLOR_BLUE;
-        } else if(areEqual(argv[argi], "-a")) {
+        } else if (are_equal(argv[argi], "-a")) {
             accent = COLOR_AQUA;
-        } else if(areEqual(argv[argi], "-p")) {
+        } else if (are_equal(argv[argi], "-p")) {
             accent = COLOR_PINK;
-        } else if(areEqual(argv[argi], "-c")) {
+        } else if (are_equal(argv[argi], "-c")) {
             accent = COLOR_CYAN;
-        } else if(areEqual(argv[argi], "-y")) {
+        } else if (are_equal(argv[argi], "-y")) {
             accent = COLOR_YELLOW;
-        } else if(areEqual(argv[argi], "-w")) {
+        } else if (are_equal(argv[argi], "-w")) {
             accent = COLOR_WHITE;
-        } else if(areEqual(argv[argi], "--chance")) {
-            symbolChangeChance = atoi(argv[argi + 1]);
-            if(symbolChangeChance == 0) {
+        } else if (are_equal(argv[argi], "--rainbow")) {
+            is_rainbow = 1;
+        } else if (are_equal(argv[argi], "--chance")) {
+            symb_change_change = atoi(argv[argi + 1]);
+            if (symb_change_change == 0) {
                 printf("Symbol change chance is incorrect\n");
                 return 0;
             }
             argi++;
-        } else if(areEqual(argv[argi], "--speed")) {
-            refreshDelay = atoi(argv[argi + 1]) * 1000;
-            if(refreshDelay == 0) {
+        } else if (are_equal(argv[argi], "--speed")) {
+            refreshDelay = atoi(argv[argi + 1])* 1000;
+            if (refreshDelay == 0) {
                 printf("Speed is incorrect\n");
                 return 0;
             }
             argi++;
-        } else if(areEqual(argv[argi], "--length")) {
+        } else if (are_equal(argv[argi], "--length")) {
             dlLength = atoi(argv[argi + 1]);
-            if(dlLength <= 0) {
+            if (dlLength <= 0) {
                 printf("Length is incorrect\n");
                 return 0;
             }
@@ -227,43 +229,43 @@ int main(int argc, char ** argv) {
         L'¦', L'╌', L'ç', L'ﾘ', L'ｸ'
     };
 
-    //Random numbers seed
+    // Random numbers seed
     srandom(time(NULL));
     setlocale(LC_ALL, "");
   
     int max_x = 0, max_y = 0;
-    getTermSize(&max_x, &max_y);
-    struct Digital * digitals = malloc(sizeof(struct Digital) * max_x);
+    get_term_size(&max_x, &max_y);
+    struct Line* digitals = malloc(sizeof(struct Line)* max_x);
     system("tput reset && tput civis");
  
-    if(!dlLengthSet) dlLength = max_y / 2; 
+    if (!dlLengthSet) dlLength = max_y / 2; 
 
-    // init digital lines columns
-    for(int j = 1; j <= max_x; j++) {
+    //  init digital lines columns
+    for (int j = 1; j <= max_x; j++) {
         int l = get_rand_in_range(5, dlLength);
-        digitals[j - 1] = (struct Digital){j,  get_rand_in_range(-30, -1), l, 0, get_rand_in_range(1, 6), generateRain(chars, l)};
+        digitals[j - 1] = (struct Line){j,  get_rand_in_range(-30, -1), l, 0, get_rand_in_range(1, 6), generate_rain(chars, l)};
     }
 
-    signal(SIGINT, intHandler);
+    signal(SIGINT, int_handler);
     while(running) {
-        //resize check
+        // resize check
         int new_x = 0, new_y = 0;
-        getTermSize(&new_x, &new_y);
-        if(max_x != new_x || max_y != new_y) {
+        get_term_size(&new_x, &new_y);
+        if (max_x != new_x || max_y != new_y) {
             system("tput reset && tput civis");
             max_y = new_y;
             max_x = new_x;
-            if(!dlLengthSet) dlLength = max_y / 2;
-            digitals = (struct Digital *) realloc(digitals, sizeof(struct Digital) * max_x);
-            for(int j = 1; j <= max_x; j++) {
+            if (!dlLengthSet) dlLength = max_y / 2;
+            digitals = (struct Line*) realloc(digitals, sizeof(struct Line)* max_x);
+            for (int j = 1; j <= max_x; j++) {
                 int l = get_rand_in_range(5, dlLength);
-                digitals[j - 1] = (struct Digital){j,  get_rand_in_range(-30, -1), l, 0, get_rand_in_range(1, 6), generateRain(chars, l)};
+                digitals[j - 1] = (struct Line){j,  get_rand_in_range(-30, -1), l, 0, get_rand_in_range(1, 6), generate_rain(chars, l)};
             }
         }
-        //going through all digital lines
-        for(int i = 0; i < max_x; i++) {
-            //print them, if line ended, redefine it
-            moveAndDrawDL(digitals + i, max_y, chars, accent, symbolChangeChance);
+        // going through all digital lines
+        for (int i = 0; i < max_x; i++) {
+            // print them, if line ended, redefine it
+            line_tick(digitals + i, max_y, chars, accent, symb_change_change, is_rainbow);
         }
         usleep(refreshDelay);
     }
